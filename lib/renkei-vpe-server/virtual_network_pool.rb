1ef95cb51e6fdc5522d98b173cd49d7d87ce1bef
popadmin <popadmin@7b9ae982-e271-4c2d-9391-7b1ded103672>
2011-04-19 16:03:24 +0000

    refactored

--- lib/renkei-vpe-server/virtual_network_pool.rb
+++ lib/renkei-vpe-server/virtual_network_pool.rb
@@ -17,30 +17,24 @@ module RenkeiVPE
     # Implement xml rpc functions
     ##########################################################################
 
-    # return information about host pool.
+    # return information about virtual network pool.
     # +session+   string that represents user session
     # +return[0]+ true or false whenever is successful or not
     # +return[1]+ if an error occurs this is error message,
     #             if successful this is the information string
     def info(session)
-      authenticate(session) do
-        begin
-          pool_e = REXML::Element.new('VNET_POOL')
-          RenkeiVPE::Model::VirtualNetwork.each do |vnet|
-            vnet_e = VirtualNetwork.to_xml_element(vnet, session)
-            pool_e.add(vnet_e)
-          end
-          doc = REXML::Document.new
-          doc.add(pool_e)
-          rc = [true, doc.to_s]
-        rescue => e
-          rc = [false, e.message]
+      task('rvpe.vpool.info', session) do
+        pool_e = REXML::Element.new('VNET_POOL')
+        RenkeiVPE::Model::VirtualNetwork.each do |vnet|
+          vnet_e = VirtualNetwork.to_xml_element(vnet, session)
+          pool_e.add(vnet_e)
         end
-
-        log_result('rvpe.vnpool.info', rc)
-        return rc
+        doc = REXML::Document.new
+        doc.add(pool_e)
+        [true, doc.to_s]
       end
     end
+
   end
 end
 
