7a2ffdb30b56d8fa7fba5d19dfd656a2d0aab901
popadmin <popadmin@7b9ae982-e271-4c2d-9391-7b1ded103672>
2011-05-09 03:22:53 +0000

    add /etc/profile.d/*.sh,*.csh used in renkei-vpe

---
+++ share/system_files/etc/profile.d/renkei-vpe.csh
@@ -0,0 +1,2 @@
+setenv RVPE_LOCATION /usr/rpop/renkei-vpe
+setenv PATH $RVPE_LOCATION/bin:$RVPE_LOCATION/sbin:$PATH
---
+++ share/system_files/etc/profile.d/renkei-vpe.sh
@@ -0,0 +1,2 @@
+export RVPE_LOCATION=/usr/rpop/renkei-vpe
+export PATH=$RVPE_LOCATION/bin:$RVPE_LOCATION/sbin:$PATH
