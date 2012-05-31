f2e5ac4fd02749a73b5657cc1245f1dcc3ff88f6
Shinichiro Takizawa <s.takizawa@gmail.com>
2012-05-31 20:27:07 +0900

    Update shell environment files
    - modify to set MANPATH in shell environment files

--- share/system_files/etc/profile.d/renkei-vpe.csh
+++ share/system_files/etc/profile.d/renkei-vpe.csh
@@ -1,3 +1,4 @@
 setenv RVPE_LOCATION /usr/rpop/renkei-vpe
 setenv PATH $RVPE_LOCATION/bin:$RVPE_LOCATION/sbin:$PATH
+setenv MANPATH $RVPE_LOCATION/share/man:$MANPATH
 setenv RVPE_XMLRPC http://localhost:3111/
--- share/system_files/etc/profile.d/renkei-vpe.sh
+++ share/system_files/etc/profile.d/renkei-vpe.sh
@@ -1,3 +1,4 @@
 export RVPE_LOCATION=/usr/rpop/renkei-vpe
 export PATH=$RVPE_LOCATION/bin:$RVPE_LOCATION/sbin:$PATH
+export MANPATH=$RVPE_LOCATION/share/man:$MANPATH
 export RVPE_XMLRPC=http://localhost:3111/
